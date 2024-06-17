d5cf266e662ebbc07b5ae2aa0029c57350429d97
u-sri2002 <usrimart@gmail.com>
2024-06-17 21:26:29 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+
+snake = Turtle()
+snake.fillcolor("white")
+snake.shape("square")
+
+
+
+window = Screen()
+window.screensize(700, 800)
+window.bgcolor("#151515")
+window.exitonclick()
