3249eb033cfd4d8c9dfa051dd8568d0661f3cf4a
u-sri2002 <usrimart@gmail.com>
2024-06-18 19:35:56 +0530

    create snake_classes.py & add functions to move the snake

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from turtle import Turtle, Screen
-
-
-snake = Turtle()
-snake.fillcolor("white")
-snake.shape("square")
-
-
+from snake_classes import Snake
+import time
 
 window = Screen()
-window.screensize(700, 800)
-window.bgcolor("#151515")
+window.setup(width=800, height=700)
+window.bgcolor("#161616")
+window.title("SNAKE GAME")
+window.tracer(0)
+
+snake = Snake()
+
+
+window.listen()
+window.onkey(snake.up, "Up")
+window.onkey(snake.down, "Down")
+window.onkey(snake.left, "Left")
+window.onkey(snake.right, "Right")
+
+
+is_game_on = True
+while is_game_on:
+
+    snake.move()
+    window.update()
+    time.sleep(.08)
+
 window.exitonclick()
+
