58447c352a84804510ea09e566f0be6e8916f18f
u-sri2002 <usrimart@gmail.com>
2024-06-20 23:20:50 +0530

    create a Wall class to add a wall to screen

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ window = Screen()
 window.setup(width=800, height=700)
 window.bgcolor("#161616")
 window.title("SNAKE GAME")
+window.tracer(0)
 
 
 MAX_WIDTH_X = 400
@@ -11,28 +12,33 @@ MIN_WIDTH_X = -400
 MAX_HEIGHT_Y = 350
 MIN_HEIGHT_Y = -350
 
+class Wall(Turtle):
 
-line = Turtle()
-line.pencolor("white")
-line.pensize(10)
-line.penup()
+    def __init__(self) :
 
-line.setpos(MIN_WIDTH_X, MAX_HEIGHT_Y)
-line.pendown()
-line.goto(MAX_WIDTH_X, MAX_HEIGHT_Y)
-line.goto(MAX_WIDTH_X, MIN_HEIGHT_Y)
-line.goto(MIN_WIDTH_X, MIN_HEIGHT_Y)
-line.goto(MIN_WIDTH_X, MAX_HEIGHT_Y)
+        super().__init__()
+
+        self.pencolor("white")
+        self.hideturtle()
+        self.pensize(10)
+        self.penup()
+        self.setpos(MIN_WIDTH_X, MAX_HEIGHT_Y)
+        self.create_wall()
+
+    def create_wall(self):
+
+        self.pendown()
+        self.goto(MAX_WIDTH_X, MAX_HEIGHT_Y)
+        self.goto(MAX_WIDTH_X, MIN_HEIGHT_Y)
+        self.goto(MIN_WIDTH_X, MIN_HEIGHT_Y)
+        self.goto(MIN_WIDTH_X, MAX_HEIGHT_Y)
+
+        
+
+wall = Wall()
+wall.create_wall()
+window.update()
 
 
 
-
-
-
-
-
-
-
-
-
-window.exitonclick()
+window.mainloop()
