376f2c6c20852081790521e289d7cd9891554225
u-sri2002 <usrimart@gmail.com>
2024-06-22 20:09:31 +0530

    add mouth to snake

--- snake_blocks.py
+++ snake_blocks.py
@@ -7,7 +7,7 @@ LEFT = 180
 RIGHT = 0
 
 # Colors for snake decoration
-SNAKE_COLORS = ["#76B041", "#77D353", "#98FB98"]  # Example gradient colors
+SNAKE_COLORS = ["#76B041", "#77D353", "#98FB98"]  
 
 # Creating Snake blocks 
 class Snake:
@@ -18,6 +18,7 @@ class Snake:
         self.head = self.snake[0]
         self.left_eye = self.create_eye((-5, 5))
         self.right_eye = self.create_eye((5, 5))
+        self.mouth = self.create_mouth((0,0))
 
     def create_snake(self):
         for i in range(3):
@@ -28,12 +29,13 @@ class Snake:
             blocks.setpos(x=-(i * 20), y=0)
             self.snake.append(blocks)
 
+    # creating eyes for snake 
     def create_eye(self, position):
         eye = Turtle()
         eye.penup()
         eye.shape("circle")
-        eye.color("white")
-        eye.shapesize(0.2, 0.2)  # Adjust the size as needed
+        eye.color("black")
+        eye.shapesize(0.2, 0.2)  
         eye.setpos(self.head.xcor() + position[0], self.head.ycor() + position[1])
         return eye
 
@@ -41,6 +43,20 @@ class Snake:
         self.left_eye.setpos(self.head.xcor() - 5, self.head.ycor() + 5)
         self.right_eye.setpos(self.head.xcor() + 5, self.head.ycor() + 5)
 
+    # creating a mouth for snake 
+    def create_mouth(self, position):
+        mouth = Turtle()
+        mouth.penup()
+        mouth.shape("circle")
+        mouth.color("red")
+        mouth.shapesize(0.2, 0.5)  
+        mouth.setpos(self.head.xcor() + position[0], self.head.ycor() + position[1])
+        return mouth
+
+    def update_mouth_position(self):
+        self.mouth.setpos(self.head.xcor() , self.head.ycor() - 4)
+
+    # expand the snake 
     def expand(self):
         blocks = Turtle()
         blocks.penup()
@@ -58,6 +74,7 @@ class Snake:
 
         self.head.forward(FORWARD_MOVES)
         self.update_eyes_position()
+        self.update_mouth_position()
 
     def up(self):
         if self.head.heading() != DOWN:
